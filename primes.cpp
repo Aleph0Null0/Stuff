06d8e019b35541fdec16fa2b4438baeea13ef9a4
Aleph0Null0 <64078009+Aleph0Null0@users.noreply.github.com>
2020-09-20 13:37:44 -0400

    Add files via upload

---
+++ primes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int nextPrime (int n) {
+    while true {
+        int factor = 2
+        while (factor<pow(n,1/2)) {
+            
+        }
+
+    }
+}
+int isPrime(int n) {
+    int i=0
+    while (i<pow(n,1/2))) {
+
+    }
+}
+std::vector<int> primeNumbers = {2,3,5,7,11,13,17,19,23,29,31,37}
---
+++ tests.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int fibonacci(int n, int total=0) {
+    int prev = n;
+    if (total >= 4000000) {
+        return value-n;
+    if (n<1) {
+        return 0;
+    }
+    else {
+        total += n;
+    }
+        return fibonacci(n+prev,total);
+    }
+}
+int main() {
+    std::cout << fibonacci(3);
+}
